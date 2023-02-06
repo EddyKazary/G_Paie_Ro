01a58529195d4f6a2c0e7295e2c1060d3e5cf9b0
Eddy RABEMANANTSOA <ed.rabe@ROCPMAJ.lan>
2023-02-06 09:42:57 +0300

    first commit

---
+++ src/main/java/com/ro/gpaie/marin/GPaieMApplication.java
@@ -0,0 +1,13 @@
+package com.ro.gpaie.marin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPaieMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GPaieMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ro/gpaie/marin/GPaieMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ro.gpaie.marin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GPaieMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
